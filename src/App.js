90e614d29bdd67b638bcec44d4533c696269970f
HabaOleh <gabikoleh25@gmail.com>
2022-04-16 15:39:01 +0300

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import {Route, Routes,Navigate} from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import {MainLayout} from "./layouts";
 import {
     CommentsPage,
     HomePage,
-    NotFoundPage, PostCommentsPage,
-    PostsPage, SingleCommentPage,
+    NotFoundPage,
+    PostCommentsPage,
+    PostsPage,
+    SingleCommentPage,
     SinglePostPage,
     SingleUserPage,
     UserPostsPage,
@@ -13,28 +15,28 @@ import {
 
 
 function App() {
-  return (
-    <Routes>
-      <Route path={"/"} element={<MainLayout/>}>
-          <Route index element={<Navigate to={"home"}/>}/>
-          <Route path={"home"} element={<HomePage/>}/>
-          <Route path={"users"} element={<UsersPage/>}>
-              <Route path={":userId"} element={<SingleUserPage/>}>
-                  <Route path={"posts"} element={<UserPostsPage/>}/>
-              </Route>
-          </Route>
-          <Route path={"posts"} element={<PostsPage/>}>
-              <Route path={":id"} element={<SinglePostPage/>}>
-                  <Route path={"comments"} element={<PostCommentsPage/>}/>
-              </Route>
-          </Route>
-          <Route path={"comments"} element={<CommentsPage/>}>
-              <Route path={":commentId"} element={<SingleCommentPage/>}/>
-          </Route>
-          <Route path={"*"} element={<NotFoundPage/>}/>
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path={"/"} element={<MainLayout/>}>
+                <Route index element={<Navigate to={"home"}/>}/>
+                <Route path={"home"} element={<HomePage/>}/>
+                <Route path={"users"} element={<UsersPage/>}>
+                    <Route path={":userId"} element={<SingleUserPage/>}>
+                        <Route path={"posts"} element={<UserPostsPage/>}/>
+                    </Route>
+                </Route>
+                <Route path={"posts"} element={<PostsPage/>}>
+                    <Route path={":id"} element={<SinglePostPage/>}>
+                        <Route path={"comments"} element={<PostCommentsPage/>}/>
+                    </Route>
+                </Route>
+                <Route path={"comments"} element={<CommentsPage/>}>
+                    <Route path={":commentId"} element={<SingleCommentPage/>}/>
+                </Route>
+                <Route path={"*"} element={<NotFoundPage/>}/>
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
